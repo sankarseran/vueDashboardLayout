0c40de98b15a6c62c7c4ffc990a0f79fc9b7a28a
unknown <sankaralingam.psg@gmail.com>
2022-11-02 10:01:32 +0100

    First commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex w-full flex-column align-items-center justify-content-start flex-wrap">
+    <!-- first container  -->
+    <div class="flex w-full about-first-container flex-column align-items-center justify-content-start flex-wrap">
+      <div class="w-full">
+        <div class="text-center p-3 border-round-sm bg-green-500 font-bold text-white">12</div>
+      </div>
+      <div class="w-full">
+        <div class="text-center p-3 border-round-sm bg-green-500 font-bold text-white">12</div>
+      </div>
+    </div>
+    <!-- second container  -->
+    <div class="flex about-second-container w-full flex-row align-items-center justify-content-start flex-wrap">
+      <div class="second">
+        <div class="h-full text-center p-3 border-round-sm bg-yellow-500 font-bold text-gray-900">6</div>
+      </div>
+      <div class="second">
+        <div class="h-full text-center p-3 border-round-sm bg-yellow-500 font-bold text-gray-900">6</div>
+      </div>
+      <div class="second">
+        <div class="h-full text-center p-3 border-round-sm bg-yellow-500 font-bold text-gray-900">6</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  /* .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  } */
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+// import TheWelcome from "../components/TheWelcome.vue";
+</script>
+
+<template>
+  <div class="grid w-full">
+    <div class="grid w-full first-container">
+      <div class="col-12">
+        <div class="text-center p-3 border-round-sm bg-green-500 font-bold text-white">12</div>
+      </div>
+      <div class="col-12">
+        <div class="text-center p-3 border-round-sm bg-green-500 font-bold text-white">12</div>
+      </div>
+      <div class="col-12">
+        <div class="text-center p-3 border-round-sm bg-green-500 font-bold text-white">12</div>
+      </div>
+    </div>
+    <div class="grid w-full second-container">
+      <div class="col-12 md:col-12 lg:col-6">
+        <div class="text-center p-3 border-round-sm bg-yellow-500 font-bold text-gray-900">6</div>
+      </div>
+      <div class="col-12 md:col-12 lg:col-6">
+        <div class="text-center p-3 border-round-sm bg-yellow-500 font-bold text-gray-900">6</div>
+      </div>
+      <div class="col-12 md:col-12 lg:col-6">
+        <div class="text-center p-3 border-round-sm bg-yellow-500 font-bold text-gray-900">6</div>
+      </div>
+      <div class="col-12 md:col-12 lg:col-6">
+        <div class="text-center p-3 border-round-sm bg-yellow-500 font-bold text-gray-900">6</div>
+      </div>
+      <div class="col-12 md:col-12 lg:col-6">
+        <div class="text-center p-3 border-round-sm bg-yellow-500 font-bold text-gray-900">6</div>
+      </div>
+    </div>
+  </div>
+</template>
